5afbb2e8f1843b6dbe3a30c93fbff67de5ac5be5
Yes-said <bobsaid358@gmail.com>
2024-06-07 14:40:52 +0300

    orderController updated

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,9 +1,11 @@
-const { createOrder } = require("../service/order.service");
+const { createOrder, updateOrder } = require("../service/order.service");
 const orderService = require("../services/order.service.js");
 const userService = require("../services/user.service.js");
 
 
 module.exports = {
+
+    // customer order controller
 
     createOrder: async (req, res) => {
 
@@ -44,6 +46,67 @@ try {
         res.status(400).json({ error: error.message });
     } else {
         rs.status(500).json({ error: "Internal server error"});
+    }
+}
+
+    },
+
+
+    //admin order controller
+
+    deleteOrder: async (req, res) => {
+
+try {
+    const { orderId } = req.params;
+    await orderService.cancelOrder(orderId);
+    res.status(200).json({ message: `Order deleted with id ${orderId}` })
+    
+} catch (error) {
+    
+    if (error instanceof Error) {
+        res.status(400).json({ error: error.message });
+    } else {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
+
+    },
+
+
+    getAllRestaurantOrders: async (req, res) => {
+
+try {
+
+    const { restaurantId } = req.params;
+    const { order_status } = req.query;
+    const orders = await orderService.getOrdersOfRestaurant(restaurantId, order_status);
+    res.status(200).json(orders);
+} catch (error) {
+    
+    if (error instanceof Error) {
+        res.status(400).json({ error: error.message });
+    } else {
+        rse.status(400).json({error: 'Internal server error'});
+    }
+}
+
+    },
+
+
+    updateOrder: async (req, res) => {
+
+try {
+    
+const { orderId, orderStatus } = req.params;
+const order = await orderService.updateOrder(orderId, orderStatus);
+res.status(200).json(order);
+
+} catch (error) {
+ 
+    if (error instanceof Error) {
+        res.status(400).json({ error: error.message });
+    } else {
+        res.status(500).json({ error: "Internal server error"});
     }
 }
 
